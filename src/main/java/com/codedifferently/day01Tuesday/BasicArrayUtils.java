803f473840a8a83d553f31276e8558973696adc5
victor840 <victor.ugoanusi@gmail.com>
2020-11-03 20:11:56 -0500

    add int method

--- src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
+++ src/main/java/com/codedifferently/day01Tuesday/BasicArrayUtils.java
@@ -6,7 +6,8 @@ public class BasicArrayUtils {
      * @return the first element in the array
      */
     public static String getFirstElement(String[] stringArray) {
-        return null;
+
+        return stringArray[0];
     }
 
     /**
@@ -14,7 +15,8 @@ public class BasicArrayUtils {
      * @return the second element in the array
      */
     public static String getSecondElement(String[] stringArray) {
-        return null;
+
+        return stringArray[1];
     }
 
     /**
@@ -22,7 +24,13 @@ public class BasicArrayUtils {
      * @return stringArray with the elements in reverse order
      */
     public static String[] reverse(String[] stringArray) {
-        return null;
+        int counter = 0;
+        String[] reverseString = new String[stringArray.length];
+        for(int i = stringArray.length -1; i>=0;  i--){
+            reverseString[counter] =  stringArray[i];
+            counter++;
+        }
+        return reverseString;
     }
 
     /**
@@ -30,6 +38,11 @@ public class BasicArrayUtils {
      * @return String made up of the first character in each element of stringArray
      */
     public static String getFirstLetterOfEachElement(String[] stringArray) {
-        return null;
+        StringBuilder stringBuilder = new StringBuilder();
+        for(int i =0; i < stringArray.length;i++){
+            char firstLetter = stringArray[i].charAt(0);
+            stringBuilder.append(firstLetter);
+        }
+        return stringBuilder.toString();
     }
 }
