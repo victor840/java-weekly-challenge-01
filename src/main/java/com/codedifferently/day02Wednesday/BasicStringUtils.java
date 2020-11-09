1ef95d8d95b6c980c05b701775fa96f305a6bcd6
victor840 <victor.ugoanusi@gmail.com>
2020-11-08 22:40:33 -0500

    add int method

--- src/main/java/com/codedifferently/day02Wednesday/BasicStringUtils.java
+++ src/main/java/com/codedifferently/day02Wednesday/BasicStringUtils.java
@@ -11,8 +11,8 @@ public class BasicStringUtils {
         //made a reference upperCase of type String and made it an empty string.
         String upperCaseValue = "";
         // for loop designed to make the first letter in the array upper case
-        for(int i = 0; i < strArray.length; i++) {
-          upperCaseValue += Character.toUpperCase(strArray[i].charAt(0))+strArray[i].substring(1) + " ";
+        for (int i = 0; i < strArray.length; i++) {
+            upperCaseValue += Character.toUpperCase(strArray[i].charAt(0)) + strArray[i].substring(1) + " ";
         }
         //".trim" removed all leading and trailing spaces in string
         return upperCaseValue.trim();
@@ -24,23 +24,26 @@ public class BasicStringUtils {
      */
     public static String reverse(String str) {
 
-            String reverseWords = "";
-            for(int i = str.length() -1; i >= 0; i--){
-                reverseWords += str.charAt(i);
-            }
-            return reverseWords;
+        String reverse = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            reverse += str.charAt(i);
         }
+        return reverse;
+    }
 
     /**
      * @param str string input from client
      * @return string with identical contents, with each word individually in reverse order
      */
     public static String reverseWords(String str) {
+        // convert string to array using Java split method.
+        String strArray[] = str.split(" ");
+        //made a reference reverseWords of type String and made it an empty string.
         String reverseWords = "";
-        for(int i = str.length() -1; i >= 0; i--){
-            reverseWords += str.charAt(i);
+        for (int i = 0; i < strArray.length; i++) {
+            reverseWords += BasicStringUtils.reverse(strArray[i]) + " ";
         }
-        return reverseWords;
+        return reverseWords.trim();
     }
 
     /**
@@ -48,7 +51,20 @@ public class BasicStringUtils {
      * @return string with identical contents, in reverse order, with first character capitalized
      */
     public static String reverseThenCamelCase(String str) {
-        return null;
+    /*    String reversedString = BasicStringUtils.reverse(str);
+        String strArray[] = reversedString.split(" " );
+        String reverseThenCamelCase = "";
+
+        for(int i = 0; i < strArray.length; i++) {
+            reverseThenCamelCase += BasicStringUtils.camelCase(strArray[i]) + " ";
+        } */
+        String reversedString = BasicStringUtils.reverse(str);
+        String strArray[] = reversedString.split(" ");
+        String reverseThenCamelCase = "";
+        for (int i = 0; i < strArray.length; i++) {
+            reverseThenCamelCase += BasicStringUtils.camelCase(strArray[i]) + " ";
+        }
+        return reverseThenCamelCase.trim();
     }
 
 
@@ -57,7 +73,8 @@ public class BasicStringUtils {
      * @return string with identical contents excluding first and last character
      */
     public static String removeFirstAndLastCharacter(String str) {
-        return null;
+        String noFirstandlastLetter = str.substring(1, str.length() - 1);
+        return noFirstandlastLetter;
     }
 
     /**
@@ -65,6 +82,16 @@ public class BasicStringUtils {
      * @return string with identical characters, each with opposite casing
      */
     public static String invertCasing(String str) {
-        return null;
+        char[] inverseCasingArray = str.toCharArray();
+        for (int i = 0; i < inverseCasingArray.length; i++){
+            if (Character.isUpperCase(inverseCasingArray[i])){
+                inverseCasingArray[i] = Character.toLowerCase(inverseCasingArray[i]);
+            }else if(Character.isLowerCase(inverseCasingArray[i]))
+                    inverseCasingArray[i] = Character.toUpperCase(inverseCasingArray[i]);
+        }
+        str = new String(inverseCasingArray);
+
+        return str;
     }
 }
+
